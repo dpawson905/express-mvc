92876fbeb45bf44135fc8b57781cd70b4f46bc8e
Darrell Pawson <dpawson905@gmail.com>
2021-11-05 23:36:05 -0400

    refactor code to ES Modules, use listr for showing progress and cleanup

--- index.js
+++ index.js
@@ -1,10 +1,8 @@
 #!/usr/bin/env node
-
+require = require("esm")(module /*, options*/);
 const chalk = require("chalk");
 const clear = require("clear");
 const figlet = require("figlet");
-
-const setup = require("./lib/setup");
 
 clear();
 
@@ -14,9 +12,4 @@ console.log(
   )
 );
 
-try {
-  setup.setupInit();
-} catch (err) {
-  console.log(err.message);
-  process.exit();
-}
+require("./lib/files").isDirEmpty(process.cwd());
